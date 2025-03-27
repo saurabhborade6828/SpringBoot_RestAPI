c40cf537fc7f0841f78b74ac54928258adae9e33
Saurabh Borade <saurabh.borade123@gmail.com>
2025-03-28 03:24:55 +0530

    spelling corrected

--- src/test/java/org/jsp/Rest_Api/classes/HelloWorld.java
+++ src/test/java/org/jsp/Rest_Api/classes/HelloWorld.java
@@ -3,6 +3,6 @@ package org.jsp.Rest_Api.classes;
 public class HelloWorld 
 {
    public static void test(){
-      System.out.println("This is the demo method.");
+      System.out.println("This is the demo method");
    }
 }
